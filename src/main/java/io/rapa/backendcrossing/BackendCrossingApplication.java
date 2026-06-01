66e3ecb5cf4aeb7a36012446a4787f0a28ec4cb4
FA-50 <wjdtn747@naver.com>
2026-06-01 10:19:04 +0900

    [FEAT] 초기 프로젝트 설정

---
+++ src/main/java/io/rapa/backendcrossing/BackendCrossingApplication.java
@@ -0,0 +1,13 @@
+package io.rapa.backendcrossing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCrossingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendCrossingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/rapa/backendcrossing/BackendCrossingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.rapa.backendcrossing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCrossingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
