7062df813e68e2f580bdb20117482b5b7c099658
FA-50 <wjdtn747@naver.com>
2026-06-02 14:34:28 +0900

    [FEAT] AuthService / AuthServiceTest - 로그인 기능 구현 및 테스트 작성

---
+++ src/main/java/io/rapa/backendcrossing/users/domain/dto/request/AuthLoginRequest.java
@@ -0,0 +1,9 @@
+package io.rapa.backendcrossing.users.domain.dto.request;
+
+import jakarta.validation.constraints.Pattern;
+
+public record AuthLoginRequest(
+        @Pattern(regexp="^[0-9A-Za-z._%+-]+@[0-9A-Za-z.-]+\\.[A-Za-z]{2,6}$") String email,
+        String password
+) {
+}
